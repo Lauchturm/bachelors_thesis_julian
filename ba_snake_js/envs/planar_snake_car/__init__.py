9d6147fa2b6e68b304c8e819f2041e3a6f2caa09
Julian Schmitz <julian.schmitz@protonmail.com>
2018-10-06 19:19:25 +0200

    removed old scenes

--- ba_snake_js/envs/planar_snake_car/__init__.py
+++ ba_snake_js/envs/planar_snake_car/__init__.py
@@ -1,15 +1,15 @@
 from ba_snake_js.envs.planar_snake_car.planar_snake_car_base import PlanarBase
 from ba_snake_js.envs.planar_snake_car.planar_locomotion import PlanarLocomotion
 from ba_snake_js.envs.planar_snake_car.planar_direction import PlanarDirection
-from ba_snake_js.envs.planar_snake_car.planar_obstacle import PlanarObstacle
+# from ba_snake_js.envs.planar_snake_car.planar_obstacle import PlanarObstacle
 from ba_snake_js.envs.planar_snake_car.planar_enjoy_circle import PlanarDirectionEnjoyCircle, \
     PlanarLocomotionEnjoyCircle, PlanarDirectionSlowEnjoyCircle
-from ba_snake_js.envs.planar_snake_car.planar_obstacle_enjoy_5 import PlanarObstacleEnjoyFive
-from ba_snake_js.envs.planar_snake_car.planar_obstacle_enjoy_5_128 import PlanarObstacleEnjoyFiveHighRes
-from ba_snake_js.envs.planar_snake_car.planar_obstacle_2 import PlanarObstacle2
+# from ba_snake_js.envs.planar_snake_car.planar_obstacle_enjoy_5 import PlanarObstacleEnjoyFive
+# from ba_snake_js.envs.planar_snake_car.planar_obstacle_enjoy_5_128 import PlanarObstacleEnjoyFiveHighRes
+# from ba_snake_js.envs.planar_snake_car.planar_obstacle_2 import PlanarObstacle2
 from ba_snake_js.envs.planar_snake_car.planar_enjoy_line import PlanarDirectionEnjoyLine, PlanarLocomotionEnjoyLine, \
     PlanarScriptStraightEnjoyLine, PlanarDirectionSlowEnjoyLine
-from ba_snake_js.envs.planar_snake_car.planar_obstacle_3 import PlanarObstacle3
+# from ba_snake_js.envs.planar_snake_car.planar_obstacle_3 import PlanarObstacle3
 from ba_snake_js.envs.planar_snake_car.planar_direction_frequency_2 import PlanarDirectionFrequency2
-from ba_snake_js.envs.planar_snake_car.planar_direction_parquet import PlanarDirectionParquet
-from ba_snake_js.envs.planar_snake_car.planar_locomotion_parquet import PlanarLocomotionParquet
+# from ba_snake_js.envs.planar_snake_car.planar_direction_parquet import PlanarDirectionParquet
+# from ba_snake_js.envs.planar_snake_car.planar_locomotion_parquet import PlanarLocomotionParquet
